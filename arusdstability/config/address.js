28ae59dd0c507d560ce39852dbd571f54c0e7bf5
Altbee <liaojikundever@gmail.com>
2022-03-13 07:57:01 -0500

    update

--- arusdstability/config/address.js
+++ arusdstability/config/address.js
@@ -1,21 +1,21 @@
-const chainId = Number(process.env.CHAIN_ID || '43113');
+const chainId = Number(process.env.CHAIN_ID || "43113");
 
 const contracts = {
   43113: {
     // testnet
-    USDT: '',
-    arUSD: '0xF01d1b172b008d052C3dbA3A57c21FFd705d49a7',
-    pairUSDTarUSD: '',
-    arableCollateral: '0xdfAc6d1e26507815a454eAab43779A19214A6c72',
-    arableLiquidation: '0x95d6471b2435252873A5dD39cC693b2c692B3b09',
+    USDT: "0x6BFa5db24866541c8703f84eF36cC202bcd43322",
+    arUSD: "0xF01d1b172b008d052C3dbA3A57c21FFd705d49a7",
+    pairUSDTarUSD: "0x1bce2604574f6b0c5a9e504c51e1eec458274d5d",
+    arableCollateral: "0xdfAc6d1e26507815a454eAab43779A19214A6c72",
+    arableLiquidation: "0x95d6471b2435252873A5dD39cC693b2c692B3b09",
   },
   43114: {
     // mainnet
-    USDT: '',
-    arUSD: '',
-    pairUSDTarUSD: '',
-    arableCollateral: '',
-    arableLiquidation: '',
+    USDT: "",
+    arUSD: "",
+    pairUSDTarUSD: "",
+    arableCollateral: "",
+    arableLiquidation: "",
   },
 };
 
--- arusdstability/index.js
+++ arusdstability/index.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const { ethers, Contract, utils, BigNumber } = require('ethers');
-const pangolin_pair_abi = require('./abis/pangolin_pair_abi');
-const erc20_abi = require('./abis/erc20_abi');
+require("dotenv").config();
+const { ethers, Contract, utils, BigNumber } = require("ethers");
+const pangolin_pair_abi = require("./abis/pangolin_pair_abi");
+const erc20_abi = require("./abis/erc20_abi");
 
-const { setup } = require('./config/network');
+const { setup } = require("./config/network");
 
-const { waitSeconds } = require('../utils/wait');
+const { waitSeconds } = require("../utils/wait");
 
 const {
   arUSD,
   USDT,
   pairUSDTarUSD,
   liquidation,
-} = require('./config/address.js');
-const { parseEther } = require('ethers/lib/utils');
+} = require("./config/address.js");
+const { parseEther } = require("ethers/lib/utils");
 
 const web3 = setup();
 
@@ -28,7 +28,7 @@ async function approveToken(token) {
   const myAccount = account.address;
   const gasPrice = await web3.eth.getGasPrice();
 
-  console.log('initiate the contracts with abi and contract address');
+  console.log("initiate the contracts with abi and contract address");
   // initiate the contracts with abi and contract address
   const tokenContract = new web3.eth.Contract(erc20_abi, token);
 
@@ -38,12 +38,12 @@ async function approveToken(token) {
   );
   let allowance = await allowanceCall.call();
 
-  if (BigNumber.from(allowance).gte(parseEther('10000000'))) {
-    console.log('already allowed and skipping');
+  if (BigNumber.from(allowance).gte(parseEther("10000000"))) {
+    console.log("already allowed and skipping");
     return;
   }
 
-  console.log('approving token');
+  console.log("approving token");
   const approveTx = tokenContract.methods.approve(
     liquidation,
     ethers.constants.MaxUint256.toString()
@@ -55,13 +55,13 @@ async function approveToken(token) {
     gasPrice,
   });
 
-  console.log('approval finished', approveTxObj);
+  console.log("approval finished", approveTxObj);
 }
 
 async function runPriceStabilizer() {
-  console.log('----- arUSD stability maintainer, in progress code -----');
+  console.log("----- arUSD stability maintainer, in progress code -----");
   console.log(
-    '================ starting arUSD stability maintainer ================'
+    "================ starting arUSD stability maintainer ================"
   );
 
   // initiate the account
@@ -73,7 +73,7 @@ async function runPriceStabilizer() {
   const gasPrice = await web3.eth.getGasPrice();
 
   // check pair price
-  const pairContract = new Contract(pairUSDTarUSD, pangolin_pair_abi, provider);
+  const pairContract = new Contract(pairUSDTarUSD, pangolin_pair_abi);
 
   // function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
   const stables = await pairContract.getReserves();
@@ -83,15 +83,15 @@ async function runPriceStabilizer() {
 
   const priceOfArUSD = reserve0 / reserve1;
 
-  console.log('arUSD price', priceOfArUSD);
+  console.log("arUSD price", priceOfArUSD);
 
   // function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
   // bytes: data.length is greater than 0, the contract transfers the tokens and then calls the following function on the to address:
-  console.log('checking..');
+  console.log("checking..");
   if (priceOfArUSD <= 0.98) {
     // buy 10000 arUSD
-    const bytes = ethers.utils.toUtf8Bytes('2');
-    const amount0In = parseEther('10000');
+    const bytes = ethers.utils.toUtf8Bytes("2");
+    const amount0In = parseEther("10000");
     const amount1Out = 0;
     const swapTx = pairContract.swap(
       amount0In,
@@ -106,12 +106,12 @@ async function runPriceStabilizer() {
       gasPrice,
     });
 
-    console.log('swap finished', swapTxObj);
+    console.log("swap finished", swapTxObj);
   } else if (priceOfArUSD >= 1.02) {
     // sell 10000 arUSD
-    const bytes = ethers.utils.toUtf8Bytes('2');
+    const bytes = ethers.utils.toUtf8Bytes("2");
     const amount0In = 0;
-    const amount1Out = parseEther('10000');
+    const amount1Out = parseEther("10000");
 
     const swapTx = pairContract.swap(
       amount0In,
@@ -126,12 +126,12 @@ async function runPriceStabilizer() {
       gasPrice,
     });
 
-    console.log('swap finished', swapTxObj);
+    console.log("swap finished", swapTxObj);
   } else {
-    console.log('no action required for price stability');
+    console.log("no action required for price stability");
   }
   console.log(
-    '================ finishing arUSD stability maintainer ================'
+    "================ finishing arUSD stability maintainer ================"
   );
 }
 
