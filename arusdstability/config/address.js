9ffe86ef8470ac5643ea6abcde0fa0d9923f27fe
Altbee <liaojikundever@gmail.com>
2022-03-14 12:33:28 -0500

    fix swap

--- arusdstability/config/address.js
+++ arusdstability/config/address.js
@@ -8,6 +8,7 @@ const contracts = {
     pairUSDTarUSD: "0x1bce2604574f6b0c5a9e504c51e1eec458274d5d",
     arableCollateral: "0xdfAc6d1e26507815a454eAab43779A19214A6c72",
     arableLiquidation: "0x95d6471b2435252873A5dD39cC693b2c692B3b09",
+    pangolinRouter: "0x2d99abd9008dc933ff5c0cd271b88309593ab921",
   },
   43114: {
     // mainnet
@@ -16,6 +17,7 @@ const contracts = {
     pairUSDTarUSD: "",
     arableCollateral: "",
     arableLiquidation: "",
+    pangolinRouter: "0xe54ca86531e17ef3616d22ca28b0d458b6c89106",
   },
 };
 
@@ -25,6 +27,7 @@ const {
   pairUSDTarUSD,
   arableCollateral: collateral,
   arableLiquidation: liquidation,
+  pangolinRouter,
 } = contracts[chainId];
 
 exports.USDT = USDT;
@@ -32,3 +35,4 @@ exports.arUSD = arUSD;
 exports.pairUSDTarUSD = pairUSDTarUSD;
 exports.collateral = collateral;
 exports.liquidation = liquidation;
+exports.pangolinRouter = pangolinRouter;
--- arusdstability/index.js
+++ arusdstability/index.js
@@ -1,21 +1,62 @@
-require('dotenv').config();
-const { ethers, Contract, utils, BigNumber } = require('ethers');
-const pangolin_pair_abi = require('./abis/pangolin_pair_abi');
-const erc20_abi = require('./abis/erc20_abi');
+require("dotenv").config();
+const { ethers, Contract, utils, BigNumber } = require("ethers");
+const pangolin_pair_abi = require("./abis/pangolin_pair_abi");
+const pangolin_router_abi = require("./abis/pangolin_router_abi");
+const erc20_abi = require("./abis/erc20_abi");
 
-const { setup } = require('./config/network');
+const { setup } = require("./config/network");
 
-const { waitSeconds } = require('../utils/wait');
+const { waitSeconds } = require("../utils/wait");
 
 const {
   arUSD,
   USDT,
   pairUSDTarUSD,
   liquidation,
-} = require('./config/address.js');
-const { parseEther } = require('ethers/lib/utils');
+  pangolinRouter,
+} = require("./config/address.js");
+const { parseEther } = require("ethers/lib/utils");
 
 const web3 = setup();
+
+async function pairApprove(token) {
+  // initiate the account
+  const account = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY
+  );
+  await web3.eth.accounts.wallet.add(account);
+  const myAccount = account.address;
+  const gasPrice = await web3.eth.getGasPrice();
+
+  console.log("initiate the contracts with abi and contract address");
+  // initiate the contracts with abi and contract address
+  const tokenContract = new web3.eth.Contract(erc20_abi, token);
+
+  const allowanceCall = await tokenContract.methods.allowance(
+    myAccount,
+    pairUSDTarUSD
+  );
+  let allowance = await allowanceCall.call();
+
+  if (BigNumber.from(allowance).gte(parseEther("10000000"))) {
+    console.log("already allowed and skipping");
+    return;
+  }
+  //
+  console.log("approve pair");
+  const pairApproveTx = tokenContract.methods.approve(
+    pairUSDTarUSD,
+    ethers.constants.MaxUint256.toString()
+  );
+
+  const pairApproveTxObj = await pairApproveTx.send({
+    from: myAccount,
+    gasLimit: 300000,
+    gasPrice,
+  });
+
+  console.log("pair approval finished", pairApproveTxObj);
+}
 
 async function approveToken(token) {
   // initiate the account
@@ -26,7 +67,7 @@ async function approveToken(token) {
   const myAccount = account.address;
   const gasPrice = await web3.eth.getGasPrice();
 
-  console.log('initiate the contracts with abi and contract address');
+  console.log("initiate the contracts with abi and contract address");
   // initiate the contracts with abi and contract address
   const tokenContract = new web3.eth.Contract(erc20_abi, token);
 
@@ -36,12 +77,12 @@ async function approveToken(token) {
   );
   let allowance = await allowanceCall.call();
 
-  if (BigNumber.from(allowance).gte(parseEther('10000000'))) {
-    console.log('already allowed and skipping');
+  if (BigNumber.from(allowance).gte(parseEther("10000000"))) {
+    console.log("already allowed and skipping");
     return;
   }
 
-  console.log('approving token', token);
+  console.log("approving token", token);
   const approveTx = tokenContract.methods.approve(
     liquidation,
     ethers.constants.MaxUint256.toString()
@@ -52,13 +93,12 @@ async function approveToken(token) {
     gasLimit: 300000,
     gasPrice,
   });
-
-  console.log('approval finished', approveTxObj);
+  console.log("approval finished", approveTxObj);
 }
 
 async function runPriceStabilizer() {
   console.log(
-    '================ starting arUSD stability maintainer ================'
+    "================ starting arUSD stability maintainer ================"
   );
 
   // initiate the account
@@ -69,74 +109,82 @@ async function runPriceStabilizer() {
   const myAccount = account.address;
   const gasPrice = await web3.eth.getGasPrice();
 
-  console.log('----- step1 -----', gasPrice);
+  console.log("----- step1 -----", gasPrice);
   // check pair price
   const pairContract = new web3.eth.Contract(pangolin_pair_abi, pairUSDTarUSD);
+  const routerContract = new web3.eth.Contract(
+    pangolin_router_abi,
+    pangolinRouter
+  );
 
   // function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
   const getReserveCall = await pairContract.methods.getReserves();
   let reserves = await getReserveCall.call();
 
-  console.log('----- step2 -----', reserves);
+  console.log("----- step2 -----", reserves);
   const reserve0 = Number(utils.formatUnits(reserves[0], 6));
   const reserve1 = Number(utils.formatUnits(reserves[1], 18));
 
-  console.log('----- step3 -----');
+  console.log("----- step3 -----");
   const priceOfArUSD = reserve0 / reserve1;
 
-  console.log('arUSD price', priceOfArUSD);
+  console.log("arUSD price", priceOfArUSD);
 
   // function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
   // bytes: data.length is greater than 0, the contract transfers the tokens and then calls the following function on the to address:
-  console.log('checking..');
+  console.log("checking..");
   if (priceOfArUSD <= 0.98) {
     // buy 10000 arUSD
-    const bytes = ethers.utils.toUtf8Bytes('2');
-    const amount0Out = parseEther('10000');
-    const amount1Out = '0';
-    console.log('----- step4 -----');
-    const swapTx = pairContract.methods.swap(
-      amount0Out,
-      amount1Out,
+    const bytes = ethers.utils.toUtf8Bytes("2");
+    const amount0Out = parseEther("10000");
+    const amount1Out = "0";
+    console.log("----- step4 -----");
+
+    const swapTx = routerContract.methods.swapTokensForExactTokens(
+      amount0Out.toString(),
+      ethers.constants.MaxUint256.toString(),
+      [USDT, arUSD],
       myAccount,
-      bytes
+      Math.floor(Date.now() / 1000) + 5 * 60 // 5 min
     );
 
-    console.log('----- step5 -----');
+    console.log("----- step5 -----");
     const swapTxObj = await swapTx.send({
       from: myAccount,
       gasLimit: 300000,
       gasPrice,
     });
 
-    console.log('swap finished', swapTxObj);
+    console.log("swap finished", swapTxObj);
   } else if (priceOfArUSD >= 1.02) {
     // sell 10000 arUSD
-    const bytes = ethers.utils.toUtf8Bytes('2');
-    const amount0Out = '0';
-    const amount1Out = parseEther('10000');
+    const bytes = ethers.utils.toUtf8Bytes("2");
+    const amount0Out = "0";
+    const amount1Out = parseEther("10000");
 
-    console.log('----- step6 -----');
-    const swapTx = pairContract.methods.swap(
-      amount0Out,
+    console.log("----- step6 -----");
+
+    const swapTx = routerContract.methods.swapExactTokensForTokens(
       amount1Out,
+      0,
+      [arUSD, USDT],
       myAccount,
-      bytes
+      Math.floor(Date.now() / 1000) + 5 * 60 // 5 min
     );
 
-    console.log('----- step7 -----');
+    console.log("----- step7 -----");
     const swapTxObj = await swapTx.send({
       from: myAccount,
       gasLimit: 300000,
       gasPrice,
     });
 
-    console.log('swap finished', swapTxObj);
+    console.log("swap finished", swapTxObj);
   } else {
-    console.log('no action required for price stability');
+    console.log("no action required for price stability");
   }
   console.log(
-    '================ finishing arUSD stability maintainer ================'
+    "================ finishing arUSD stability maintainer ================"
   );
 }
 
@@ -144,6 +192,8 @@ async function main() {
   // approve tokens to be used for stability
   await approveToken(arUSD);
   await approveToken(USDT);
+  await pairApprove(arUSD);
+  await pairApprove(USDT);
 
   // run price stabilizer once per min
   while (1 == 1) {
