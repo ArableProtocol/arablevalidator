8016f0996eb482095ef3ab70eead0147dede5a03
gophereth <gophereth121@gmail.com>
2022-03-22 11:57:43 +1000

    update arusd stability script

--- arusdstability/config/address.js
+++ arusdstability/config/address.js
@@ -7,8 +7,8 @@ const contracts = {
     arableLiquidation: '0x32bb77cDB588C0e55324DF3bC09ea70013c9E482',
     arUSD: '0x5DaF787B6f5839956D1a50d3B9d0d7DF3FEb3328',
     USDT: '0x6BFa5db24866541c8703f84eF36cC202bcd43322',
-    pairUSDTarUSD: '0x1bce2604574f6b0c5a9e504c51e1eec458274d5d', // TODO: reset
-    pangolinRouter: '0x2d99abd9008dc933ff5c0cd271b88309593ab921', // TODO: reset if required
+    pairUSDTarUSD: '0x5e2602ab615d38a8ce38ac56705eea2749fbc40c',
+    pangolinRouter: '0x2d99abd9008dc933ff5c0cd271b88309593ab921',
   },
   43114: {
     // mainnet
