07691d7e51b862fa6c7e508125859b717958188d
gophereth <gophereth121@gmail.com>
2022-03-23 20:09:24 +1000

    set allowed oracle feeder

---
+++ oracle/utils/index.js
@@ -0,0 +1,27 @@
+const { parseEther } = require('ethers/lib/utils');
+const { setup } = require('../config/network');
+const oracle_abi = require('../submitter/abis/oracle_abi');
+const { getAddresses } = require('../config/address');
+
+const web3 = setup();
+
+exports.setAllowedProvider = async function (addr) {
+  const { oracle } = await getAddresses();
+  const account = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY
+  );
+  await web3.eth.accounts.wallet.add(account);
+  const myAccount = account.address;
+  const gasPrice = await web3.eth.getGasPrice();
+
+  const oracleContract = new web3.eth.Contract(oracle_abi, oracle);
+
+  const setAllowedProviderTx = oracleContract.methods.setAllowedProvider(addr);
+  const txObj = await setAllowedProviderTx.send({
+    from: myAccount,
+    gasLimit: web3.utils.toHex(3000000),
+    gasPrice,
+  });
+  console.log('Success setAllowedProvider!', addr, txObj.transactionHash);
+  return txObj.transactionHash;
+};
