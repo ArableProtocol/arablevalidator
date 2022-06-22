4c2c78c1c73066f5fffc237dd6dba0a6959ebb5b
Altbee <liaojikundever@gmail.com>
2022-06-22 11:34:10 -0500

    update addresses

--- arusdstability/config/address.js
+++ arusdstability/config/address.js
@@ -1,25 +1,25 @@
-const chainId = Number(process.env.CHAIN_ID || '43113');
+const chainId = Number(process.env.CHAIN_ID || "43113");
 
 const contracts = {
   43113: {
     // testnet
-    arableCollateral: '0x6b6E63b76B0A1a1289F27721aDC5fa6B28F3e99b',
-    arableLiquidation: '0x08668C65f7a1Fa40a170bA17276F2DD08c94aaBF',
-    arUSD: '0x57C5c0a1ccAC1A28f9B165d0DdF42feaa0796C25',
-    USDT: '0x6BFa5db24866541c8703f84eF36cC202bcd43322',
-    pairWAVAXarUSD: '0x1434f3a9724cdc60781d1f5bcc69a8e5316ad999',
-    pairUSDTarUSD: '0xfc2648426608965b5b6f21784200af8cb016cf70',
-    pangolinRouter: '0x2d99abd9008dc933ff5c0cd271b88309593ab921',
+    arableCollateral: "0x9D38695C52ED31E9Bd0A6ABE8b51612554B7de35",
+    arableLiquidation: "0x6119B823e3C5081Ca194c352A1001E51f933850A",
+    arUSD: "0x4b362aCEB2140540Acded484FEeF5D0238c5EF09",
+    USDT: "0x6BFa5db24866541c8703f84eF36cC202bcd43322",
+    pairWAVAXarUSD: "0x69c7547070c928cac8d5c57f9960e1598c55da0e",
+    pairUSDTarUSD: "0xa75f5b31d1c36262e7735be0f48cdc6f0f6ccf51",
+    pangolinRouter: "0x2d99abd9008dc933ff5c0cd271b88309593ab921",
   },
   43114: {
     // mainnet
-    arableCollateral: '',
-    arableLiquidation: '',
-    arUSD: '',
-    USDT: '',
-    pairWAVAXarUSD: '',
-    pairUSDTarUSD: '',
-    pangolinRouter: '0xe54ca86531e17ef3616d22ca28b0d458b6c89106',
+    arableCollateral: "",
+    arableLiquidation: "",
+    arUSD: "",
+    USDT: "",
+    pairWAVAXarUSD: "",
+    pairUSDTarUSD: "",
+    pangolinRouter: "0xe54ca86531e17ef3616d22ca28b0d458b6c89106",
   },
 };
 
--- liquidator/config/address.js
+++ liquidator/config/address.js
@@ -1,28 +1,28 @@
-const chainId = Number(process.env.CHAIN_ID || '43113');
+const chainId = Number(process.env.CHAIN_ID || "43113");
 
 const contracts = {
   43113: {
     // testnet
-    arableCollateral: '0x6b6E63b76B0A1a1289F27721aDC5fa6B28F3e99b',
-    arableLiquidation: '0x08668C65f7a1Fa40a170bA17276F2DD08c94aaBF',
-    arUSD: '0x57C5c0a1ccAC1A28f9B165d0DdF42feaa0796C25',
+    arableCollateral: "0x9D38695C52ED31E9Bd0A6ABE8b51612554B7de35",
+    arableLiquidation: "0x6119B823e3C5081Ca194c352A1001E51f933850A",
+    arUSD: "0x4b362aCEB2140540Acded484FEeF5D0238c5EF09",
     decimals: {
-      '0xd00ae08403b9bbb9124bb305c09058e32c39a48c': 18, // wavax
-      '0x23542139268078d36b75fab7b3e3deb50f038b3d': 18, // acre
-      '0x6bfa5db24866541c8703f84ef36cc202bcd43322': 6, // usdt
-      '0x7e309ea562892fcb691b4460d350144020379647': 18, // aft
+      "0xd00ae08403b9bbb9124bb305c09058e32c39a48c": 18, // wavax
+      "0x23542139268078d36b75fab7b3e3deb50f038b3d": 18, // acre
+      "0x6bfa5db24866541c8703f84ef36cc202bcd43322": 6, // usdt
+      "0x7e309ea562892fcb691b4460d350144020379647": 18, // aft
     },
   },
   43114: {
     // mainnet
-    arableCollateral: '',
-    arableLiquidation: '',
-    arUSD: '',
+    arableCollateral: "",
+    arableLiquidation: "",
+    arUSD: "",
     decimals: {
       // TODO: to be set for mainnet
-      '0xd00ae08403b9bbb9124bb305c09058e32c39a48c': 18, // wavax
-      '0x23542139268078d36b75fab7b3e3deb50f038b3d': 18, // acre
-      '0x6bfa5db24866541c8703f84ef36cc202bcd43322': 6, // usdt
+      "0xd00ae08403b9bbb9124bb305c09058e32c39a48c": 18, // wavax
+      "0x23542139268078d36b75fab7b3e3deb50f038b3d": 18, // acre
+      "0x6bfa5db24866541c8703f84ef36cc202bcd43322": 6, // usdt
     },
   },
 };
